9d36d7ffa91271bf0b44023d560df7eaf40fed07
ABiteOfSorrow <laceican@naver.com>
2023-01-09 20:43:17 +0100

    Add buttons & delete function

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import FoodList from './FoodList';
-import items from '../mock.json';
+import MockData from '../mock.json';
+import { useState } from 'react';
 
 function App() {
+    const [order, setOrder] = useState('createdAt');
+    const [listItems, setListItems] = useState(MockData);
+
+    const sortedItems = listItems.sort((a, b) => (b[order] - a[order]));
+
+    const handleNewestClick = () => setOrder('createdAt');
+    const handleCalorieClick = () => setOrder('calorie');
+    const handleDelete = (id) => {
+        const nextItem = listItems.filter((item) => item.id !== id);
+        setListItems(nextItem);
+    }
+
   return (
     <div>
-      <FoodList items={items} />
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} onDelete={handleDelete}/>
     </div>
   );
 }
