4e7db7c32e2fb96ea419cd15b1ca8929d54f5b08
ABiteOfSorrow <laceican@naver.com>
2023-01-18 20:44:48 +0100

    Add file input

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,31 +1,39 @@
 import { useState } from "react"
+import FileInput from "./FileInput";
 import "./FoodForm.css";
 
 function FoodForm() {
 
-    const [title, setTitle] = useState('');
-    const [calorie, setCalorie] = useState(0);
-    const [content, setContent] = useState('');
+    const [values, setValues] = useState({
+        title: '',
+        calorie: 0,
+        content: '',
+        imgFile: null,
+    });
 
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
+    const handleChange = (name, value) => {
+        setValues((preValues) => ({
+            ...preValues, [name]: value
+        })
+    )}
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        handleInputChange(name, value)
     }
 
-    const handleCalorieChange = (e) => {
-        const nextCalorie = Number(e.target.value) || 0;
-        setCalorie(nextCalorie);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({values})
     }
-
-    const handleContentChange = (e) => {
-        setContent(e.target.value);
-    }
-
 
     return (
-    <form className="FoodForm">
-        <input name="title" value={title} onChange={handleTitleChange} />
-        <input type="number" name="calorie" value={calorie} onChange={handleCalorieChange} />
-        <input name="content" value={content} onChange={handleContentChange} />
+    <form className="FoodForm" onSubmit={handleSubmit}>
+        <FileInput name="imgFile" value={values.imgFile} onChange={handleChange}/>
+        <input name="title" value={values.title} onChange={handleInputChange} />
+        <input type="number" name="calorie" value={values.calorie} onChange={handleInputChange} />
+        <input name="content" value={values.content} onChange={handleInputChange} />
+        <button type="submit">확인</button>
     </form>
     )
 
