eb35da0a260c377dc5b23e7d19a98cedc8c66720
ABiteOfSorrow <laceican@naver.com>
2023-01-19 20:57:24 +0100

    Add preview image function

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function FileInput({ name, value, onChange}) {
+    const [preView, setPreview] = useState();
+
     const inputRef = useRef();
 
     const handleChange = (e) => {
@@ -16,8 +18,22 @@ function FileInput({ name, value, onChange}) {
         onChange(name, null);
     }
 
+    useEffect(() => {
+
+        if(!value) return;
+        const nextPreview = URL.createObjectURL(value);
+        setPreview(nextPreview);
+
+        return () => {
+            setPreview();
+            URL.revokeObjectURL(nextPreview)        
+        }
+
+    },[value])
+
     return (
         <div>
+        <img src={preView} alt="이미지 미리보기" />
         <input type="file" onChange={handleChange} ref={inputRef}/>
         {value && <button onClick={handleClearClick}>x</button>}
         </div>
